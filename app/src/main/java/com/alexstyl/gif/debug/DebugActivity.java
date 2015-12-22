6f2f79192ab75293bcb8fd376f6aaaace9b1b53d
Alex Styl <alexstyl@alexs-mbp.home>
2015-12-22 23:45:21 +0000

    DebugScreenEnabler class enables and disables the DebugActivity

---
+++ app/src/main/java/com/alexstyl/gif/debug/DebugActivity.java
@@ -0,0 +1,16 @@
+package com.alexstyl.gif.debug;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.alexstyl.gif.R;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_debug);
+    }
+}
