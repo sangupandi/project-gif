130c71103a0ee38534a0654c71bbdd1a2edbf6de
Alex Styl <alexanderstyl@gmail.com>
2015-12-28 21:13:32 +0000

    Add OverlayManager used to managing the overlay on the screen

---
+++ app/src/main/java/com/alexstyl/gif/overlay/OverlayManager.java
@@ -0,0 +1,65 @@
+package com.alexstyl.gif.overlay;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.alexstyl.gif.R;
+
+public class OverlayManager {
+
+    public static OverlayManager newInstance(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View overlayView = inflateOverlayView(layoutInflater);
+
+        return new OverlayManager(windowManager, overlayView);
+    }
+
+    private static View inflateOverlayView(LayoutInflater layoutInflater) {
+        return layoutInflater.inflate(R.layout.layout_overlay, null, false);
+    }
+
+    private final WindowManager windowManager;
+    private final View overlayView;
+
+    public OverlayManager(WindowManager windowManager, View overlayView) {
+        this.windowManager = windowManager;
+        this.overlayView = overlayView;
+    }
+
+    public void startOverlay() {
+        if (overlayIsAttached()) {
+            return;
+        }
+        WindowManager.LayoutParams params = buildOverlayLayoutParams();
+        windowManager.addView(overlayView, params);
+    }
+
+    private WindowManager.LayoutParams buildOverlayLayoutParams() {
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                        WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
+                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT
+        );
+        layoutParams.windowAnimations = R.style.Overlay;
+        layoutParams.gravity = Gravity.CENTER;
+
+        return layoutParams;
+    }
+
+    private boolean overlayIsAttached() {
+        return overlayView.getParent() != null;
+    }
+
+    public void destroy() {
+        windowManager.removeView(overlayView);
+    }
+}
