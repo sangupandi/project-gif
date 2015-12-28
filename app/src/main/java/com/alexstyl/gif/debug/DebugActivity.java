3a0860d2c8c5aa4ab8b027528c391bac2fbf8090
Alex Styl <alexanderstyl@gmail.com>
2015-12-28 21:14:51 +0000

    Added OverlayService that displays an overlay when running

--- app/src/main/java/com/alexstyl/gif/debug/DebugActivity.java
+++ app/src/main/java/com/alexstyl/gif/debug/DebugActivity.java
@@ -6,28 +6,43 @@ import android.support.v7.widget.SwitchCompat;
 import android.widget.CompoundButton;
 
 import com.alexstyl.gif.R;
-import com.alexstyl.gif.overlay.OverlayEnabler;
+import com.alexstyl.gif.overlay.OverlayService;
+import com.alexstyl.gif.overlay.OverlayServiceHelper;
 
 public class DebugActivity extends AppCompatActivity {
 
+    private OverlayServiceHelper overlayServiceHelper;
     private SwitchCompat enableSwitch;
-    private OverlayEnabler overlayEnabler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
-        overlayEnabler = OverlayEnabler.newInstance(this);
-
+        overlayServiceHelper = OverlayServiceHelper.newInstance(this);
         enableSwitch = (SwitchCompat) findViewById(R.id.checkbox_enable);
+        setupEnableSwitch();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateOverlaySwitch();
+    }
+
+    private void updateOverlaySwitch() {
+        boolean isServiceRunning = OverlayService.IS_RUNNING;
+        enableSwitch.setChecked(isServiceRunning);
+    }
+
+    private void setupEnableSwitch() {
         enableSwitch.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                         if (isChecked) {
-                            overlayEnabler.enableService();
+                            overlayServiceHelper.enableService();
                         } else {
-                            overlayEnabler.disableService();
+                            overlayServiceHelper.disableService();
                         }
                     }
                 }
---
+++ app/src/main/java/com/alexstyl/gif/overlay/OverlayService.java
@@ -0,0 +1,51 @@
+package com.alexstyl.gif.overlay;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.alexstyl.gif.Notifier;
+
+public class OverlayService extends Service {
+
+    private OverlayManager overlayManager;
+
+    private static final int NOTIFICATION_ID = 1;
+
+    public static boolean IS_RUNNING = false;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        overlayManager = OverlayManager.newInstance(this);
+        moveToForeground();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        overlayManager.startOverlay();
+        IS_RUNNING = true;
+        return START_STICKY_COMPATIBILITY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        overlayManager.destroy();
+        IS_RUNNING = false;
+    }
+
+    private void moveToForeground() {
+        Notifier notifier = Notifier.newInstance(this);
+        Notification notification = notifier.createNotificationForOverlayService();
+        startForeground(NOTIFICATION_ID, notification);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
